8d18a5328b66f29fde32270ccce20ad6f5f15e84
Noah Cain <nmcain@gmail.com>
2020-10-02 11:46:39 -0700

    Implement YaruSwitch, design also needs improvement

---
+++ lib/YaruRadio.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'yaru.dart';
+import 'package:flutter/src/material/constants.dart';
+import 'package:flutter/src/material/debug.dart';
+import 'package:flutter/src/material/material_state.dart';
+import 'package:flutter/src/material/theme.dart';
+import 'package:flutter/src/material/theme_data.dart';
+import 'package:flutter/src/material/toggleable.dart';
+
+const double _kOuterRadius = 8.0;
+const double _kInnerRadius = 4.5;
+
+class YaruRadio<T> extends StatefulWidget {
+  const YaruRadio({
+    Key key,
+    @required this.value,
+    @required this.groupValue,
+    @required this.onChanged,
+    this.mouseCursor,
+    this.toggleable = false,
+    this.activeColor,
+    this.focusColor,
+    this.hoverColor,
+    this.materialTapTargetSize,
+    this.visualDensity,
+    this.focusNode,
+    this.autofocus = false,
+  })  : assert(autofocus != null),
+        assert(toggleable != null),
+        super(key: key);
+
+  final T value;
+
+  final T groupValue;
+
+  final ValueChanged<T> onChanged;
+
+  final MouseCursor mouseCursor;
+
+  final bool toggleable;
+
+  final Color activeColor;
+
+  final MaterialTapTargetSize materialTapTargetSize;
+
+  final VisualDensity visualDensity;
+
+  final Color focusColor;
+
+  final Color hoverColor;
+
+  final FocusNode focusNode;
+
+  final bool autofocus;
+
+  @override
+  _YaruRadioState<T> createState() => _YaruRadioState<T>();
+}
+
+class _YaruRadioState<T> extends State<YaruRadio<T>>
+    with TickerProviderStateMixin {
+  bool get enabled => widget.onChanged != null;
+  Map<Type, Action<Intent>> _actionMap;
+
+  @override
+  void initState() {
+    super.initState();
+    _actionMap = <Type, Action<Intent>>{
+      ActivateIntent: CallbackAction<ActivateIntent>(
+        onInvoke: _actionHandler,
+      ),
+    };
+  }
+
+  void _actionHandler(ActivateIntent intent) {
+    if (widget.onChanged != null) {
+      widget.onChanged(widget.value);
+    }
+    final RenderObject renderObject = context.findRenderObject();
+    renderObject.sendSemanticsEvent(const TapSemanticEvent());
+  }
+
+  bool _focused = false;
+  void _handleHighlightChanged(bool focused) {
+    if (_focused != focused) {
+      setState(() {
+        _focused = focused;
+      });
+    }
+  }
+
+  bool _hovering = false;
+  void _handleHoverChanged(bool hovering) {
+    if (_hovering != hovering) {
+      setState(() {
+        _hovering = hovering;
+      });
+    }
+  }
+
+  Color _getInactiveColor(ThemeData themeData) {
+    return enabled ? themeData.unselectedWidgetColor : themeData.disabledColor;
+  }
+
+  void _handleChanged(bool selected) {
+    if (selected == null) {
+      widget.onChanged(null);
+      return;
+    }
+    if (selected) {
+      widget.onChanged(widget.value);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMaterial(context));
+    final ThemeData themeData = Theme.of(context);
+    Size size;
+    switch (widget.materialTapTargetSize ?? themeData.materialTapTargetSize) {
+      case MaterialTapTargetSize.padded:
+        size = const Size(
+            2 * kRadialReactionRadius + 8.0, 2 * kRadialReactionRadius + 8.0);
+        break;
+      case MaterialTapTargetSize.shrinkWrap:
+        size = const Size(2 * kRadialReactionRadius, 2 * kRadialReactionRadius);
+        break;
+    }
+    size +=
+        (widget.visualDensity ?? themeData.visualDensity).baseSizeAdjustment;
+    final BoxConstraints additionalConstraints = BoxConstraints.tight(size);
+    final bool selected = widget.value == widget.groupValue;
+    final MouseCursor effectiveMouseCursor =
+        MaterialStateProperty.resolveAs<MouseCursor>(
+      widget.mouseCursor ?? MaterialStateMouseCursor.clickable,
+      <MaterialState>{
+        if (!enabled) MaterialState.disabled,
+        if (_hovering) MaterialState.hovered,
+        if (_focused) MaterialState.focused,
+        if (selected) MaterialState.selected,
+      },
+    );
+
+    return FocusableActionDetector(
+      actions: _actionMap,
+      focusNode: widget.focusNode,
+      autofocus: widget.autofocus,
+      mouseCursor: effectiveMouseCursor,
+      enabled: enabled,
+      onShowFocusHighlight: _handleHighlightChanged,
+      onShowHoverHighlight: _handleHoverChanged,
+      child: Builder(
+        builder: (BuildContext context) {
+          return _YaruRadioRenderObjectWidget(
+            selected: selected,
+            activeColor: widget.activeColor ?? themeData.toggleableActiveColor,
+            inactiveColor: _getInactiveColor(themeData),
+            focusColor: widget.focusColor ?? themeData.focusColor,
+            hoverColor: widget.hoverColor ?? themeData.hoverColor,
+            onChanged: enabled ? _handleChanged : null,
+            toggleable: widget.toggleable,
+            additionalConstraints: additionalConstraints,
+            vsync: this,
+            hasFocus: _focused,
+            hovering: _hovering,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _YaruRadioRenderObjectWidget extends LeafRenderObjectWidget {
+  const _YaruRadioRenderObjectWidget({
+    Key key,
+    @required this.selected,
+    @required this.activeColor,
+    @required this.inactiveColor,
+    @required this.focusColor,
+    @required this.hoverColor,
+    @required this.additionalConstraints,
+    this.onChanged,
+    @required this.toggleable,
+    @required this.vsync,
+    @required this.hasFocus,
+    @required this.hovering,
+  })  : assert(selected != null),
+        assert(activeColor != null),
+        assert(inactiveColor != null),
+        assert(vsync != null),
+        assert(toggleable != null),
+        super(key: key);
+
+  final bool selected;
+  final bool hasFocus;
+  final bool hovering;
+  final Color inactiveColor;
+  final Color activeColor;
+  final Color focusColor;
+  final Color hoverColor;
+  final ValueChanged<bool> onChanged;
+  final bool toggleable;
+  final TickerProvider vsync;
+  final BoxConstraints additionalConstraints;
+
+  @override
+  _RenderYaruRadio createRenderObject(BuildContext context) => _RenderYaruRadio(
+        value: selected,
+        activeColor: activeColor,
+        inactiveColor: inactiveColor,
+        focusColor: focusColor,
+        hoverColor: hoverColor,
+        onChanged: onChanged,
+        tristate: toggleable,
+        vsync: vsync,
+        additionalConstraints: additionalConstraints,
+        hasFocus: hasFocus,
+        hovering: hovering,
+      );
+
+  @override
+  void updateRenderObject(BuildContext context, _RenderYaruRadio renderObject) {
+    renderObject
+      ..value = selected
+      ..activeColor = activeColor
+      ..inactiveColor = inactiveColor
+      ..focusColor = focusColor
+      ..hoverColor = hoverColor
+      ..onChanged = onChanged
+      ..tristate = toggleable
+      ..additionalConstraints = additionalConstraints
+      ..vsync = vsync
+      ..hasFocus = hasFocus
+      ..hovering = hovering;
+  }
+}
+
+class _RenderYaruRadio extends RenderToggleable {
+  _RenderYaruRadio({
+    bool value,
+    Color activeColor,
+    Color inactiveColor,
+    Color focusColor,
+    Color hoverColor,
+    ValueChanged<bool> onChanged,
+    bool tristate,
+    BoxConstraints additionalConstraints,
+    @required TickerProvider vsync,
+    bool hasFocus,
+    bool hovering,
+  }) : super(
+          value: value,
+          activeColor: activeColor,
+          inactiveColor: inactiveColor,
+          focusColor: focusColor,
+          hoverColor: hoverColor,
+          onChanged: onChanged,
+          tristate: tristate,
+          additionalConstraints: additionalConstraints,
+          vsync: vsync,
+          hasFocus: hasFocus,
+          hovering: hovering,
+        );
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    final Canvas canvas = context.canvas;
+
+    paintRadialReaction(canvas, offset, size.center(Offset.zero));
+
+    final Offset center = (offset & size).center;
+    final Color YaruRadioColor =
+        onChanged != null ? yaruLightAubergine : inactiveColor;
+
+    final Paint paint = Paint()
+      ..color = Color.lerp(inactiveColor, YaruRadioColor, position.value)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0;
+    canvas.drawCircle(center, _kOuterRadius, paint);
+
+    if (!position.isDismissed) {
+      paint.style = PaintingStyle.fill;
+      canvas.drawCircle(center, _kInnerRadius * position.value, paint);
+    }
+  }
+
+  @override
+  void describeSemanticsConfiguration(SemanticsConfiguration config) {
+    super.describeSemanticsConfiguration(config);
+    config
+      ..isInMutuallyExclusiveGroup = true
+      ..isChecked = value == true;
+  }
+}
