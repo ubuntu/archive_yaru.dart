e2f6984ab2ee3252052fc05615f95e170cc8b667
Noah Cain <nmcain@gmail.com>
2020-10-02 11:46:12 -0700

    Implement basic switch (NEEDS DESIGN IMPROVEMENT)

---
+++ lib/YaruSwitch.dart
@@ -0,0 +1,443 @@
+import 'dart:ui' show lerpDouble;
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'yaru.dart';
+import 'package:flutter/src/cupertino/thumb_painter.dart';
+
+class YaruSwitch extends StatefulWidget {
+  const YaruSwitch({
+    Key key,
+    @required this.value,
+    @required this.onChanged,
+    this.activeColor,
+    this.trackColor,
+    this.dragStartBehavior = DragStartBehavior.start,
+  })  : assert(value != null),
+        assert(dragStartBehavior != null),
+        super(key: key);
+
+  final bool value;
+
+  final ValueChanged<bool> onChanged;
+
+  final Color activeColor;
+
+  final Color trackColor;
+
+  final DragStartBehavior dragStartBehavior;
+
+  @override
+  _YaruSwitchState createState() => _YaruSwitchState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(FlagProperty('value',
+        value: value, ifTrue: 'on', ifFalse: 'off', showName: true));
+    properties.add(ObjectFlagProperty<ValueChanged<bool>>(
+        'onChanged', onChanged,
+        ifNull: 'disabled'));
+  }
+}
+
+class _YaruSwitchState extends State<YaruSwitch> with TickerProviderStateMixin {
+  TapGestureRecognizer _tap;
+  HorizontalDragGestureRecognizer _drag;
+
+  AnimationController _positionController;
+  CurvedAnimation position;
+
+  AnimationController _reactionController;
+  Animation<double> _reaction;
+
+  bool get isInteractive => widget.onChanged != null;
+
+  bool needsPositionAnimation = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tap = TapGestureRecognizer()
+      ..onTapDown = _handleTapDown
+      ..onTapUp = _handleTapUp
+      ..onTap = _handleTap
+      ..onTapCancel = _handleTapCancel;
+    _drag = HorizontalDragGestureRecognizer()
+      ..onStart = _handleDragStart
+      ..onUpdate = _handleDragUpdate
+      ..onEnd = _handleDragEnd
+      ..dragStartBehavior = widget.dragStartBehavior;
+
+    _positionController = AnimationController(
+      duration: _kToggleDuration,
+      value: widget.value ? 1.0 : 0.0,
+      vsync: this,
+    );
+    position = CurvedAnimation(
+      parent: _positionController,
+      curve: Curves.linear,
+    );
+    _reactionController = AnimationController(
+      duration: _kReactionDuration,
+      vsync: this,
+    );
+    _reaction = CurvedAnimation(
+      parent: _reactionController,
+      curve: Curves.ease,
+    );
+  }
+
+  @override
+  void didUpdateWidget(YaruSwitch oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _drag.dragStartBehavior = widget.dragStartBehavior;
+
+    if (needsPositionAnimation || oldWidget.value != widget.value)
+      _resumePositionAnimation(isLinear: needsPositionAnimation);
+  }
+
+  void _resumePositionAnimation({bool isLinear = true}) {
+    needsPositionAnimation = false;
+    position
+      ..curve = isLinear ? null : Curves.ease
+      ..reverseCurve = isLinear ? null : Curves.ease.flipped;
+    if (widget.value)
+      _positionController.forward();
+    else
+      _positionController.reverse();
+  }
+
+  void _handleTapDown(TapDownDetails details) {
+    if (isInteractive) needsPositionAnimation = false;
+    _reactionController.forward();
+  }
+
+  void _handleTap() {
+    if (isInteractive) {
+      widget.onChanged(!widget.value);
+      _emitVibration();
+    }
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    if (isInteractive) {
+      needsPositionAnimation = false;
+      _reactionController.reverse();
+    }
+  }
+
+  void _handleTapCancel() {
+    if (isInteractive) _reactionController.reverse();
+  }
+
+  void _handleDragStart(DragStartDetails details) {
+    if (isInteractive) {
+      needsPositionAnimation = false;
+      _reactionController.forward();
+      _emitVibration();
+    }
+  }
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    if (isInteractive) {
+      position
+        ..curve = null
+        ..reverseCurve = null;
+      final double delta = details.primaryDelta / _kTrackInnerLength;
+      switch (Directionality.of(context)) {
+        case TextDirection.rtl:
+          _positionController.value -= delta;
+          break;
+        case TextDirection.ltr:
+          _positionController.value += delta;
+          break;
+      }
+    }
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    setState(() {
+      needsPositionAnimation = true;
+    });
+
+    if (position.value >= 0.5 != widget.value) widget.onChanged(!widget.value);
+    _reactionController.reverse();
+  }
+
+  void _emitVibration() {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.iOS:
+        HapticFeedback.lightImpact();
+        break;
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (needsPositionAnimation) _resumePositionAnimation();
+    return Opacity(
+      opacity: widget.onChanged == null ? _kYaruSwitchDisabledOpacity : 1.0,
+      child: _YaruSwitchRenderObjectWidget(
+        value: widget.value,
+        activeColor: widget.activeColor ?? yaruLightAubergine,
+        trackColor: widget.trackColor ?? Colors.grey.withOpacity(0.5),
+        onChanged: widget.onChanged,
+        textDirection: Directionality.of(context),
+        state: this,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _tap.dispose();
+    _drag.dispose();
+
+    _positionController.dispose();
+    _reactionController.dispose();
+    super.dispose();
+  }
+}
+
+class _YaruSwitchRenderObjectWidget extends LeafRenderObjectWidget {
+  const _YaruSwitchRenderObjectWidget({
+    Key key,
+    this.value,
+    this.activeColor,
+    this.trackColor,
+    this.onChanged,
+    this.textDirection,
+    this.state,
+  }) : super(key: key);
+
+  final bool value;
+  final Color activeColor;
+  final Color trackColor;
+  final ValueChanged<bool> onChanged;
+  final _YaruSwitchState state;
+  final TextDirection textDirection;
+
+  @override
+  _RenderYaruSwitch createRenderObject(BuildContext context) {
+    return _RenderYaruSwitch(
+      value: value,
+      activeColor: activeColor,
+      trackColor: trackColor,
+      onChanged: onChanged,
+      textDirection: textDirection,
+      state: state,
+    );
+  }
+
+  @override
+  void updateRenderObject(
+      BuildContext context, _RenderYaruSwitch renderObject) {
+    renderObject
+      ..value = value
+      ..activeColor = activeColor
+      ..trackColor = trackColor
+      ..onChanged = onChanged
+      ..textDirection = textDirection;
+  }
+}
+
+const double _kTrackWidth = 51.0;
+const double _kTrackHeight = 26.0;
+const double _kTrackRadius = _kTrackHeight / 2.0;
+const double _kTrackInnerStart = _kTrackHeight / 2.0;
+const double _kTrackInnerEnd = _kTrackWidth - _kTrackInnerStart;
+const double _kTrackInnerLength = _kTrackInnerEnd - _kTrackInnerStart;
+const double _kSwitchWidth = 30.0;
+const double _kSwitchHeight = 21.0;
+
+const double _kYaruSwitchDisabledOpacity = 0.5;
+
+const Duration _kReactionDuration = Duration(milliseconds: 300);
+const Duration _kToggleDuration = Duration(milliseconds: 200);
+
+class _RenderYaruSwitch extends RenderConstrainedBox {
+  _RenderYaruSwitch({
+    @required bool value,
+    @required Color activeColor,
+    @required Color trackColor,
+    ValueChanged<bool> onChanged,
+    @required TextDirection textDirection,
+    @required _YaruSwitchState state,
+  })  : assert(value != null),
+        assert(activeColor != null),
+        assert(state != null),
+        _value = value,
+        _activeColor = activeColor,
+        _trackColor = trackColor,
+        _onChanged = onChanged,
+        _textDirection = textDirection,
+        _state = state,
+        super(
+            additionalConstraints: const BoxConstraints.tightFor(
+                width: _kSwitchWidth, height: _kSwitchHeight)) {
+    state.position.addListener(markNeedsPaint);
+    state._reaction.addListener(markNeedsPaint);
+  }
+
+  final _YaruSwitchState _state;
+
+  bool get value => _value;
+  bool _value;
+  set value(bool value) {
+    assert(value != null);
+    if (value == _value) return;
+    _value = value;
+    markNeedsSemanticsUpdate();
+  }
+
+  Color get activeColor => _activeColor;
+  Color _activeColor;
+  set activeColor(Color value) {
+    assert(value != null);
+    if (value == _activeColor) return;
+    _activeColor = value;
+    markNeedsPaint();
+  }
+
+  Color get trackColor => _trackColor;
+  Color _trackColor;
+  set trackColor(Color value) {
+    assert(value != null);
+    if (value == _trackColor) return;
+    _trackColor = value;
+    markNeedsPaint();
+  }
+
+  ValueChanged<bool> get onChanged => _onChanged;
+  ValueChanged<bool> _onChanged;
+  set onChanged(ValueChanged<bool> value) {
+    if (value == _onChanged) return;
+    final bool wasInteractive = isInteractive;
+    _onChanged = value;
+    if (wasInteractive != isInteractive) {
+      markNeedsPaint();
+      markNeedsSemanticsUpdate();
+    }
+  }
+
+  TextDirection get textDirection => _textDirection;
+  TextDirection _textDirection;
+  set textDirection(TextDirection value) {
+    assert(value != null);
+    if (_textDirection == value) return;
+    _textDirection = value;
+    markNeedsPaint();
+  }
+
+  bool get isInteractive => onChanged != null;
+
+  @override
+  bool hitTestSelf(Offset position) => true;
+
+  @override
+  void handleEvent(PointerEvent event, BoxHitTestEntry entry) {
+    assert(debugHandleEvent(event, entry));
+    if (event is PointerDownEvent && isInteractive) {
+      _state._drag.addPointer(event);
+      _state._tap.addPointer(event);
+    }
+  }
+
+  @override
+  void describeSemanticsConfiguration(SemanticsConfiguration config) {
+    super.describeSemanticsConfiguration(config);
+
+    if (isInteractive) config.onTap = _state._handleTap;
+
+    config.isEnabled = isInteractive;
+    config.isToggled = _value;
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    final Canvas canvas = context.canvas;
+
+    final double currentValue = _state.position.value;
+    final double currentReactionValue = _state._reaction.value;
+
+    double visualPosition;
+    switch (textDirection) {
+      case TextDirection.rtl:
+        visualPosition = 1.0 - currentValue;
+        break;
+      case TextDirection.ltr:
+        visualPosition = currentValue;
+        break;
+    }
+
+    final Paint paint = Paint()
+      ..color = Color.lerp(trackColor, activeColor, currentValue);
+
+    final Rect trackRect = Rect.fromLTWH(
+      offset.dx + (size.width - _kTrackWidth) / 2.0,
+      offset.dy + (size.height - _kTrackHeight) / 2.0,
+      _kTrackWidth,
+      _kTrackHeight,
+    );
+    final RRect trackRRect = RRect.fromRectAndRadius(
+        trackRect, const Radius.circular(_kTrackRadius));
+    canvas.drawRRect(trackRRect, paint);
+
+    final double currentThumbExtension =
+        CupertinoThumbPainter.extension * currentReactionValue;
+    final double thumbLeft = lerpDouble(
+      trackRect.left + _kTrackInnerStart - CupertinoThumbPainter.radius,
+      trackRect.left +
+          _kTrackInnerEnd -
+          CupertinoThumbPainter.radius -
+          currentThumbExtension,
+      visualPosition,
+    );
+    final double thumbRight = lerpDouble(
+      trackRect.left +
+          _kTrackInnerStart +
+          CupertinoThumbPainter.radius +
+          currentThumbExtension,
+      trackRect.left + _kTrackInnerEnd + CupertinoThumbPainter.radius,
+      visualPosition,
+    );
+    final double thumbCenterY = offset.dy + size.height / 2.0;
+    final Rect thumbBounds = Rect.fromLTRB(
+      thumbLeft,
+      thumbCenterY - CupertinoThumbPainter.radius,
+      thumbRight,
+      thumbCenterY + CupertinoThumbPainter.radius,
+    );
+
+    context
+        .pushClipRRect(needsCompositing, Offset.zero, thumbBounds, trackRRect,
+            (PaintingContext innerContext, Offset offset) {
+      const CupertinoThumbPainter.switchThumb()
+          .paint(innerContext.canvas, thumbBounds);
+    });
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder description) {
+    super.debugFillProperties(description);
+    description.add(FlagProperty('value',
+        value: value, ifTrue: 'checked', ifFalse: 'unchecked', showName: true));
+    description.add(FlagProperty('isInteractive',
+        value: isInteractive,
+        ifTrue: 'enabled',
+        ifFalse: 'disabled',
+        showName: true,
+        defaultValue: true));
+  }
+}
