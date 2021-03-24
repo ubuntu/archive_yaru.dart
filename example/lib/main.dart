db81f964b840d6ea3f4d84b957332f20d8669097
elbeicktalat <talatelbeick@gmail.com>
2021-03-24 19:15:08 +0100

    add colors example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -292,93 +292,36 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             const SizedBox(height: 15),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.orange,
-              child: Center(
-                  child: Text('yaruUbuntuOrange',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
+            Text('Yaru Colors', style: Theme.of(context).textTheme.headline4),
+            const SizedBox(height: 15.0),
+            Row(
+              children: [
+                Expanded(
+                    child: colorPaletteExample('orange', yaru.Colors.orange)),
+                SizedBox(width: 25.0),
+                Expanded(
+                    child: colorPaletteExample(
+                        'lightAubergine', yaru.Colors.lightAubergine)),
+                SizedBox(width: 25.0),
+                Expanded(
+                    child: colorPaletteExample(
+                        'midAubergine', yaru.Colors.midAubergine)),
+              ],
             ),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.lightAubergine,
-              child: Center(
-                  child: Text('yaruLightAubergine',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
-            ),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.midAubergine,
-              child: Center(
-                  child: Text('yaruMidAubergine',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
-            ),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.darkAubergine,
-              child: Center(
-                  child: Text('yaruDarkAubergine',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
-            ),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.canonicalAubergine,
-              child: Center(
-                  child: Text('yaruCanonicalAubergine',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
-            ),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.warmGrey,
-              child: Center(
-                  child: Text('yaruWarmGrey',
-                      style: TextStyle(
-                          color: yaru.Colors.textGrey,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
-            ),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.coolGrey,
-              child: Center(
-                  child: Text('yaruCoolGrey',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
-            ),
-            Container(
-              width: 250.0,
-              height: 50.0,
-              color: yaru.Colors.textGrey,
-              child: Center(
-                  child: Text('yaruTextGrey',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w300))),
+            Divider(height: 50.0, color: Colors.black),
+            Row(
+              children: [
+                Expanded(
+                    child: colorPaletteExample(
+                        'canonicalAubergine', yaru.Colors.canonicalAubergine)),
+                SizedBox(width: 25.0),
+                Expanded(
+                    child: colorPaletteExample(
+                        'darkAubergine', yaru.Colors.darkAubergine)),
+                SizedBox(width: 25.0),
+                Expanded(
+                    child: colorPaletteExample('warmGrey', yaru.Colors.warmGrey)),
+              ],
             ),
           ],
         ),
@@ -388,6 +331,55 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'This button does nothing',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  Widget colorPaletteExample(String colorName, MaterialColor color) {
+    Map<String, MaterialColor> _color = {colorName: color};
+    int _shade = 0;
+    TextStyle _textStyle(int _shade) => TextStyle(
+          color: (_color.values.first[_shade].computeLuminance() > 0.4)
+              ? Colors.black
+              : Colors.white,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w400,
+        );
+    List<Widget> _colorItem = [];
+    for (int i = 0; i < 5; i++) {
+      _shade += 100;
+      _colorItem.add(Container(
+        height: 45.0,
+        color: _color.values.first[_shade],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 12,
+                child: Text(
+                  _color.keys.first + '[$_shade]',
+                  style: _textStyle(_shade),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Text(
+                  '#' +
+                      _color.values.first[_shade].value
+                          .toRadixString(16)
+                          .substring(2)
+                          .toUpperCase(),
+                  style: _textStyle(_shade),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
+    return Column(
+      children: _colorItem,
     );
   }
 }
