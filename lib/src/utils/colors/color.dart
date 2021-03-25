e7c61714abb162680b56f9a3c2ae8f524d994770
Francesco Iapicca <iapicca@gmail.com>
2021-03-25 22:37:04 +0200

    fix colors name and clean up

---
+++ lib/src/utils/colors/color.dart
@@ -0,0 +1,56 @@
+import 'dart:ui';
+import '../../../yaru.dart';
+
+// Ubuntu color palette `https://design.ubuntu.com/brand/colour-palette/`
+
+class YaruColor {
+  const YaruColor._();
+  static const bakersChocolate = Color(YaruHex.bakersChocolate);
+  static const black = Color(YaruHex.black);
+  static const bouquet = Color(YaruHex.bouquet);
+  static const bouquet2 = Color(YaruHex.bouquet2);
+  static const bonJour = Color(YaruHex.bonJour);
+  static const bonJour2 = Color(YaruHex.bonJour2);
+  static const burntSienna = Color(YaruHex.burntSienna);
+  static const burntUmber = Color(YaruHex.burntUmber);
+  static const canonicalAubergine = Color(YaruHex.canonicalAubergine);
+  static const cannonPink = Color(YaruHex.cannonPink);
+  static const cannonPink2 = Color(YaruHex.cannonPink2);
+  static const cloud = Color(YaruHex.cloud);
+  static const coolGrey = Color(YaruHex.coolGrey);
+  static const darkAubergine = Color(YaruHex.darkAubergine);
+  static const darkSalmon = Color(YaruHex.darkSalmon);
+  static const desertStorm = Color(YaruHex.desertStorm);
+  static const desertStorm2 = Color(YaruHex.desertStorm2);
+  static const disabledGreyDark = Color(YaruHex.disabledGreyDark);
+  static const eggplant = Color(YaruHex.eggplant);
+  static const errorColor = Color(YaruHex.errorColor);
+  static const falcon = Color(YaruHex.falcon);
+  static const green = Color(YaruHex.green);
+  static const lightAubergine = Color(YaruHex.lightAubergine);
+  static const lilac = Color(YaruHex.lilac);
+  static const lily = Color(YaruHex.lily);
+  static const lily2 = Color(YaruHex.lily2);
+  static const lola = Color(YaruHex.lola);
+  static const lola2 = Color(YaruHex.lola2);
+  static const lola3 = Color(YaruHex.lola3);
+  static const manhattan = Color(YaruHex.manhattan);
+  static const midAubergine = Color(YaruHex.midAubergine);
+  static const mountbattenPink = Color(YaruHex.mountbattenPink);
+  static const oldLavender = Color(YaruHex.oldLavender);
+  static const paleSlate = Color(YaruHex.paleSlate);
+  static const pippin = Color(YaruHex.pippin);
+  static const prim = Color(YaruHex.prim);
+  static const red = Color(YaruHex.red);
+  static const roseWhite = Color(YaruHex.roseWhite);
+  static const rust = Color(YaruHex.rust);
+  static const sealBrown = Color(YaruHex.sealBrown);
+  static const shadyLady = Color(YaruHex.shadyLady);
+  static const softPeach = Color(YaruHex.softPeach);
+  static const textGrey = Color(YaruHex.textGrey);
+  static const tide = Color(YaruHex.tide);
+  static const ubuntuOrange = Color(YaruHex.ubuntuOrange);
+  static const warmGrey = Color(YaruHex.warmGrey);
+  static const westar = Color(YaruHex.westar);
+  static const white = Color(YaruHex.white);
+}
---
+++ lib/src/utils/colors/swatch.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart' show MaterialColor;
+import '../../../yaru.dart';
+
+class YaruSwatch {
+  const YaruSwatch._();
+
+  static const ubuntuOrange = MaterialColor(
+    YaruHex.ubuntuOrange,
+    {
+      50: YaruColor.roseWhite,
+      100: YaruColor.pippin,
+      200: YaruColor.manhattan,
+      300: YaruColor.darkSalmon,
+      400: YaruColor.burntSienna,
+      500: YaruColor.ubuntuOrange,
+      600: YaruColor.rust,
+      700: YaruColor.burntUmber,
+      800: YaruColor.bakersChocolate,
+      900: YaruColor.sealBrown,
+    },
+  );
+
+  static const lightAubergine = MaterialColor(
+    YaruHex.lightAubergine,
+    {
+      50: YaruColor.prim,
+      100: YaruColor.lola,
+      200: YaruColor.lilac,
+      300: YaruColor.bouquet,
+      400: YaruColor.cannonPink,
+      500: YaruColor.lightAubergine,
+      600: YaruColor.rust,
+      700: YaruColor.burntUmber,
+      800: YaruColor.bakersChocolate,
+      900: YaruColor.sealBrown,
+    },
+  );
+
+  static const midAubergine = MaterialColor(
+    YaruHex.midAubergine,
+    {
+      50: YaruColor.bonJour,
+      100: YaruColor.lola2,
+      200: YaruColor.lily,
+      300: YaruColor.mountbattenPink,
+      400: YaruColor.falcon,
+      500: YaruColor.midAubergine,
+      600: YaruColor.rust,
+      700: YaruColor.burntUmber,
+      800: YaruColor.bakersChocolate,
+      900: YaruColor.sealBrown,
+    },
+  );
+
+  static const canonicalAubergine = MaterialColor(
+    YaruHex.canonicalAubergine,
+    {
+      50: YaruColor.softPeach,
+      100: YaruColor.lola3,
+      200: YaruColor.lily2,
+      300: YaruColor.bouquet2,
+      400: YaruColor.cannonPink2,
+      500: YaruColor.canonicalAubergine,
+      600: YaruColor.rust,
+      700: YaruColor.burntUmber,
+      800: YaruColor.bakersChocolate,
+      900: YaruColor.sealBrown,
+    },
+  );
+
+  static const darkAubergine = MaterialColor(
+    YaruHex.darkAubergine,
+    {
+      50: YaruColor.bonJour2,
+      100: YaruColor.paleSlate,
+      200: YaruColor.shadyLady,
+      300: YaruColor.oldLavender,
+      400: YaruColor.eggplant,
+      500: YaruColor.darkAubergine,
+      600: YaruColor.rust,
+      700: YaruColor.burntUmber,
+      800: YaruColor.bakersChocolate,
+      900: YaruColor.sealBrown,
+    },
+  );
+
+  static const warmGrey = MaterialColor(
+    YaruHex.darkAubergine,
+    {
+      50: YaruColor.desertStorm,
+      100: YaruColor.desertStorm2,
+      200: YaruColor.westar,
+      300: YaruColor.cloud,
+      400: YaruColor.tide,
+      500: YaruColor.warmGrey,
+      600: YaruColor.rust,
+      700: YaruColor.burntUmber,
+      800: YaruColor.bakersChocolate,
+      900: YaruColor.sealBrown,
+    },
+  );
+}
