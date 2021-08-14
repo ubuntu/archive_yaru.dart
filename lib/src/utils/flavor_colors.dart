11c49f3550183702ab001fe6407a440ef1d30910
Joshua Peisach <itzswirlz2020@outlook.com>
2021-08-14 07:29:11 -0400

    Add colors for the official Ubuntu flavors

---
+++ lib/src/utils/flavor_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart' show Color, MaterialColor;
+
+class FlavorColors {
+  FlavorColors._();
+
+  static const Color kubuntuBlue = Color(0xFF0079c1);
+  static const Color lubuntuBlue = Color(0xFF0068c8);
+  static const Color ubuntuBudgieGray = Color(0xFF404522);
+  static const Color ubuntuMATEGreen = Color(0xFF73af59);
+  static const Color ubuntuStudioBlue = Color(0xFF009bf9);
+  static const Color xubuntuBlue = Color(0xFF0044aa);
+}
