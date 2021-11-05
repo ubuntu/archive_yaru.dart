9f3c47721eedddfae90f73e75a49db456608f31b
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-11-05 13:57:28 +0100

    Add a tabbar theme and update example
    - we did not theme the tabbar but we do theme the appbar which was making the tabbar have white text in the light theme. Adding a TabBarTheme fixes this
    - Updated the example to visualise a TabBar and TabBarView and split the controls section

--- example/linux/my_application.cc
+++ example/linux/my_application.cc
@@ -40,14 +40,19 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "yaru_test");
+    gtk_header_bar_set_title(header_bar, "Yaru Theme Widget Factory");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "yaru_test");
+    gtk_window_set_title(window, "Yaru Theme Widget Factory");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+   GdkGeometry geometry;
+  geometry.min_width = 600;
+  geometry.min_height = 700;
+  gtk_window_set_geometry_hints(window, nullptr, &geometry, GDK_HINT_MIN_SIZE);
+
+  gtk_window_set_default_size(window, 600, 700);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
