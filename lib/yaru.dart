005be72b3975d70877135ea3ebad6db037ab5b1a
elbeicktalat <talatelbeick@gmail.com>
2021-11-19 18:27:12 +0100

    export all Themes

--- lib/yaru.dart
+++ lib/yaru.dart
@@ -2,4 +2,18 @@ library yaru;
 
 export 'package:yaru/src/colors/flavor_colors.dart';
 export 'package:yaru/src/colors/yaru_colors.dart';
+export 'package:yaru/src/themes/budgie/yaru_ubuntu_budgie_dark.dart';
+export 'package:yaru/src/themes/budgie/yaru_ubuntu_budgie_light.dart';
+export 'package:yaru/src/themes/default/yaru_dark.dart';
+export 'package:yaru/src/themes/default/yaru_light.dart';
+export 'package:yaru/src/themes/kubuntu/yaru_kubuntu_dark.dart';
+export 'package:yaru/src/themes/kubuntu/yaru_kubuntu_light.dart';
+export 'package:yaru/src/themes/lubuntu/yaru_lubuntu_dark.dart';
+export 'package:yaru/src/themes/lubuntu/yaru_lubuntu_light.dart';
+export 'package:yaru/src/themes/mate/yaru_mate_dark.dart';
+export 'package:yaru/src/themes/mate/yaru_mate_light.dart';
+export 'package:yaru/src/themes/studio/yaru_ubuntu_studio_dark.dart';
+export 'package:yaru/src/themes/studio/yaru_ubuntu_studio_light.dart';
+export 'package:yaru/src/themes/xubuntu/yaru_xubuntu_dark.dart';
+export 'package:yaru/src/themes/xubuntu/yaru_xubuntu_light.dart';
 export 'package:yaru/src/yaru_theme.dart';
