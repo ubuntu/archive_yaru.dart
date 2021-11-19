91d0a4bdc7af829ac344e1e81da9481883122880
elbeicktalat <talatelbeick@gmail.com>
2021-11-19 12:20:52 +0100

    implement YaruMateLight theme

--- lib/src/themes/mate/yaru_mate_light.dart
+++ lib/src/themes/mate/yaru_mate_light.dart
@@ -1,112 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/src/colors/flavor_colors.dart';
 import 'package:yaru/src/colors/yaru_colors.dart';
+import 'package:yaru/src/contracts/yaru_lighter.dart';
 import 'package:yaru/src/text/text_theme.dart';
-import 'package:yaru/src/themes/common_themes.dart';
 
-const _primaryColor = FlavorColors.ubuntuMateGreen;
+class YaruMateLight extends YaruLighter {
+  final _primaryColor = FlavorColors.ubuntuMateGreen;
 
-final _lightColorScheme = ColorScheme.fromSwatch(
-  primarySwatch: _primaryColor,
-  primaryColorDark: YaruColors.coolGrey,
-  accentColor: _primaryColor,
-  cardColor: Colors.white,
-  backgroundColor: YaruColors.porcelain,
-  errorColor: YaruColors.red,
-  brightness: Brightness.light,
-);
-
-final yaruMateLight = ThemeData(
-    tabBarTheme: TabBarTheme(labelColor: _lightColorScheme.onSurface),
-    brightness: Brightness.light,
-    primaryColor: _lightColorScheme.primary,
-    primaryColorBrightness:
-        ThemeData.estimateBrightnessForColor(_lightColorScheme.primary),
-    canvasColor: _lightColorScheme.background,
-    scaffoldBackgroundColor: _lightColorScheme.background,
-    bottomAppBarColor: _lightColorScheme.surface,
-    cardColor: _lightColorScheme.surface,
-    dividerColor: _lightColorScheme.onSurface.withOpacity(0.12),
-    backgroundColor: _lightColorScheme.background,
-    dialogBackgroundColor: _lightColorScheme.background,
-    errorColor: _lightColorScheme.error,
-    textTheme: textTheme,
-    indicatorColor: _lightColorScheme.secondary,
-    applyElevationOverlayColor: false,
-    colorScheme: _lightColorScheme,
-    buttonTheme: buttonThemeData,
-    outlinedButtonTheme: outlinedButtonThemeData,
-    elevatedButtonTheme:
-        getElevatedButtonThemeData(Brightness.light, _primaryColor),
-    textButtonTheme: textButtonThemeData,
-    switchTheme: _switchStyleLight,
-    checkboxTheme: _checkStyleLight,
-    radioTheme: _radioStyleLight,
-    appBarTheme: appBarLightTheme,
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: _primaryColor,
-    ),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      selectedItemColor: _lightColorScheme.primary,
-      unselectedItemColor: YaruColors.coolGrey,
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(),
-    ));
-
-Color _getSwitchThumbColorLight(Set<MaterialState> states) {
-  if (states.contains(MaterialState.disabled)) {
-    return YaruColors.warmGrey.shade200;
-  } else {
-    if (states.contains(MaterialState.selected)) {
-      return _primaryColor;
-    } else {
+  @override
+  Color checkboxColor(Set<MaterialState> states) {
+    if (!states.contains(MaterialState.disabled)) {
       return Colors.white;
-    }
-  }
-}
-
-Color _getSwitchTrackColorLight(Set<MaterialState> states) {
-  if (states.contains(MaterialState.disabled)) {
-    return YaruColors.warmGrey.shade200;
-  } else {
-    if (states.contains(MaterialState.selected)) {
-      return _primaryColor.withAlpha(180);
-    } else {
-      return YaruColors.warmGrey.shade300;
-    }
-  }
-}
-
-final _switchStyleLight = SwitchThemeData(
-  thumbColor: MaterialStateProperty.resolveWith(_getSwitchThumbColorLight),
-  trackColor: MaterialStateProperty.resolveWith(_getSwitchTrackColorLight),
-);
-
-Color _getCheckFillColorLight(Set<MaterialState> states) {
-  if (!states.contains(MaterialState.disabled)) {
-    if (states.contains(MaterialState.selected)) {
-      return _primaryColor;
     }
     return YaruColors.warmGrey;
   }
-  return YaruColors.warmGrey.shade300;
-}
 
-Color _getCheckColorLight(Set<MaterialState> states) {
-  if (!states.contains(MaterialState.disabled)) {
-    return Colors.white;
+  @override
+  Color checkboxFillColor(Set<MaterialState> states) {
+    if (!states.contains(MaterialState.disabled)) {
+      if (states.contains(MaterialState.selected)) {
+        return _primaryColor;
+      }
+      return YaruColors.warmGrey;
+    }
+    return YaruColors.warmGrey.shade300;
   }
-  return YaruColors.warmGrey;
+
+  @override
+  CheckboxThemeData checkboxThemeData() {
+    return CheckboxThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(2),
+      ),
+      fillColor: MaterialStateProperty.resolveWith(checkboxFillColor),
+      checkColor: MaterialStateProperty.resolveWith(checkboxColor),
+    );
+  }
+
+  @override
+  ColorScheme colorScheme() {
+    return ColorScheme.fromSwatch(
+      primarySwatch: _primaryColor,
+      primaryColorDark: YaruColors.coolGrey,
+      accentColor: _primaryColor,
+      cardColor: Colors.white,
+      backgroundColor: YaruColors.porcelain,
+      errorColor: YaruColors.red,
+      brightness: Brightness.light,
+    );
+  }
+
+  @override
+  RadioThemeData radioThemeData() {
+    return RadioThemeData(
+      fillColor: MaterialStateProperty.resolveWith(checkboxFillColor),
+    );
+  }
+
+  @override
+  SwitchThemeData switchThemeData() {
+    return SwitchThemeData(
+      thumbColor: MaterialStateProperty.resolveWith(switchThumbColor),
+      trackColor: MaterialStateProperty.resolveWith(switchTrackColor),
+    );
+  }
+
+  @override
+  Color switchThumbColor(Set<MaterialState> states) {
+    if (states.contains(MaterialState.disabled)) {
+      return YaruColors.warmGrey.shade200;
+    } else {
+      if (states.contains(MaterialState.selected)) {
+        return _primaryColor;
+      } else {
+        return Colors.white;
+      }
+    }
+  }
+
+  @override
+  Color switchTrackColor(Set<MaterialState> states) {
+    if (states.contains(MaterialState.disabled)) {
+      return YaruColors.warmGrey.shade200;
+    } else {
+      if (states.contains(MaterialState.selected)) {
+        return _primaryColor.withAlpha(180);
+      } else {
+        return YaruColors.warmGrey.shade300;
+      }
+    }
+  }
+
+  @override
+  ThemeData themeData() {
+    return ThemeData(
+      tabBarTheme: TabBarTheme(labelColor: colorScheme().onSurface),
+      brightness: Brightness.light,
+      primaryColor: colorScheme().primary,
+      primaryColorBrightness: ThemeData.estimateBrightnessForColor(
+        colorScheme().primary,
+      ),
+      canvasColor: colorScheme().background,
+      scaffoldBackgroundColor: colorScheme().background,
+      bottomAppBarColor: colorScheme().surface,
+      cardColor: colorScheme().surface,
+      dividerColor: colorScheme().onSurface.withOpacity(0.12),
+      backgroundColor: colorScheme().background,
+      dialogBackgroundColor: colorScheme().background,
+      errorColor: colorScheme().error,
+      textTheme: textTheme,
+      indicatorColor: colorScheme().secondary,
+      applyElevationOverlayColor: false,
+      colorScheme: colorScheme(),
+      buttonTheme: buttonThemeData(),
+      outlinedButtonTheme: outlinedButtonThemeData(),
+      elevatedButtonTheme: elevatedButtonThemeData(
+        Brightness.light,
+        _primaryColor,
+      ),
+      textButtonTheme: textButtonThemeData(),
+      switchTheme: switchThemeData(),
+      checkboxTheme: checkboxThemeData(),
+      radioTheme: radioThemeData(),
+      appBarTheme: appBarTheme(),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: _primaryColor,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        selectedItemColor: colorScheme().primary,
+        unselectedItemColor: YaruColors.coolGrey,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
 }
-
-final _checkStyleLight = CheckboxThemeData(
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(2),
-  ),
-  fillColor: MaterialStateProperty.resolveWith(_getCheckFillColorLight),
-  checkColor: MaterialStateProperty.resolveWith(_getCheckColorLight),
-);
-
-final _radioStyleLight = RadioThemeData(
-    fillColor: MaterialStateProperty.resolveWith(_getCheckFillColorLight));
