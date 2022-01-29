ea5be5ee3c613020e1481a2fe6d4a1630feef1c7
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2022-01-29 12:53:20 +0100

    Detach appbar from yaru colors (#126)
    * detach AppBarTheme from YaruColors
    
    - use helper functions to create AppBarThemes based on a colorScheme
    - remove debug label

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,6 +19,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Yaru Example',
       theme: theme,
+      debugShowCheckedModeBanner: false,
       home: HomePage(
           themeChanged: (themeName) => setState(() {
                 if (themeName == 'Yaru-light') {
