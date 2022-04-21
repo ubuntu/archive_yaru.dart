57d12d849889433c2dc8f6b97d163699113e1492
J-P Nurmi <jp.nurmi@canonical.com>
2022-04-22 00:00:52 +0200

    Regenerate material colors

--- lib/src/colors/flavor_colors.dart
+++ lib/src/colors/flavor_colors.dart
@@ -5,97 +5,91 @@ class FlavorColors {
 
   // Kubuntu
   static const _kubuntuBluePrimaryValue = 0xFF0079C1;
-  static const kubuntuBlue =
-      MaterialColor(_kubuntuBluePrimaryValue, <int, Color>{
-    50: Color(0xFF80BCE0),
-    100: Color(0xFF66AFDA),
-    200: Color(0xFF4DA1D4),
-    300: Color(0xFF3394CD),
-    400: Color(0xFF1A86C7),
-    500: Color(_kubuntuBluePrimaryValue),
-    600: Color(0xFF006dae),
-    700: Color(0xFF00619a),
-    800: Color(0xFF005587),
-    900: Color(0xFF004974),
+  static const kubuntuBlue = MaterialColor(_kubuntuBluePrimaryValue, {
+    50: Color(0xFFE1F6FE),
+    100: Color(0xFFB4E6FD),
+    200: Color(0xFF82D6FC),
+    300: Color(0xFF50C6FA),
+    400: Color(0xFF29B9F9),
+    500: Color(0xFF01ADF8),
+    600: Color(0xFF009EE9),
+    700: Color(0xFF008BD5),
+    800: Color(_kubuntuBluePrimaryValue),
+    900: Color(0xFF005A9F),
   });
 
   // Lubuntu
-  static const _lubuntuBluePrimaryValue = 0xFF0068c8;
-  static const lubuntuBlue =
-      MaterialColor(_lubuntuBluePrimaryValue, <int, Color>{
-    50: Color(0xFF80b4e4),
-    100: Color(0xFF66a4de),
-    200: Color(0xFF4d95d9),
-    300: Color(0xFF3386d3),
-    400: Color(0xFF1a77ce),
-    500: Color(_lubuntuBluePrimaryValue),
-    600: Color(0xFF005eb4),
-    700: Color(0xFF0053a0),
-    800: Color(0xFF00498c),
-    900: Color(0xFF003e78),
+  static const _lubuntuBluePrimaryValue = 0xFF0068C8;
+  static const lubuntuBlue = MaterialColor(_lubuntuBluePrimaryValue, {
+    50: Color(0xFFE3F2FE),
+    100: Color(0xFFBBDFFD),
+    200: Color(0xFF8ECCFD),
+    300: Color(0xFF60B7FC),
+    400: Color(0xFF39A8FC),
+    500: Color(0xFF0499FB),
+    600: Color(0xFF048BED),
+    700: Color(0xFF0178DA),
+    800: Color(_lubuntuBluePrimaryValue),
+    900: Color(0xFF0049A9),
   });
 
   // Ubuntu Budgie
-  static const _ubuntuBudgieBluePrimaryValue = 0xFF2196f3;
-  static const ubuntuBudgieBlue =
-      MaterialColor(_ubuntuBudgieBluePrimaryValue, <int, Color>{
-    50: Color(0xFFe3f2fd),
-    100: Color(0xFFbbdefb),
-    200: Color(0xFF90caf9),
-    300: Color(0xFF64b5f6),
-    400: Color(0xFF42a5f5),
+  static const _ubuntuBudgieBluePrimaryValue = 0xFF2196F3;
+  static const ubuntuBudgieBlue = MaterialColor(_ubuntuBudgieBluePrimaryValue, {
+    50: Color(0xFFE3F2FD),
+    100: Color(0xFFBBDEFB),
+    200: Color(0xFF90C9F9),
+    300: Color(0xFF63B4F6),
+    400: Color(0xFF42A4F5),
     500: Color(_ubuntuBudgieBluePrimaryValue),
-    600: Color(0xFF1e88e5),
-    700: Color(0xFF1976d2),
-    800: Color(0xFF1565c0),
-    900: Color(0xFF0d47a1)
+    600: Color(0xFF1F87E5),
+    700: Color(0xFF1A75D2),
+    800: Color(0xFF1764C0),
+    900: Color(0xFF1045A1),
   });
 
   // Ubuntu MATE
   static const _ubuntuMateGreenPrimaryValue = 0xFF73AF59;
-  static const ubuntuMateGreen =
-      MaterialColor(_ubuntuMateGreenPrimaryValue, <int, Color>{
-    50: Color(0xFFb9d7ac),
-    100: Color(0xFFabcf9b),
-    200: Color(0xFF9dc78b),
-    300: Color(0xFF8fbf7a),
-    400: Color(0xFF81b76a),
-    500: Color(_ubuntuMateGreenPrimaryValue),
-    600: Color(0xFF689e50),
-    700: Color(0xFF5c8c47),
-    800: Color(0xFF517a3e),
-    900: Color(0xFF456935),
+  static const ubuntuMateGreen = MaterialColor(_ubuntuMateGreenPrimaryValue, {
+    50: Color(0xFFEFF8EB),
+    100: Color(0xFFD8ECCE),
+    200: Color(0xFFBEDFAE),
+    300: Color(0xFFA5D28F),
+    400: Color(0xFF92C978),
+    500: Color(0xFF80BF62),
+    600: Color(_ubuntuMateGreenPrimaryValue),
+    700: Color(0xFF619B4E),
+    800: Color(0xFF508745),
+    900: Color(0xFF346433),
   });
 
   // Ubuntu Studio Blue
   static const _ubuntuStudioBluePrimaryValue = 0xFF009BF9;
-  static const ubuntuStudioBlue =
-      MaterialColor(_ubuntuStudioBluePrimaryValue, <int, Color>{
-    50: Color(0xFF66c3fb),
-    100: Color(0xFF66c3fb),
-    200: Color(0xFF4db9fb),
-    300: Color(0xFF33affa),
-    400: Color(0xFF1aa5fa),
+  static const ubuntuStudioBlue = MaterialColor(_ubuntuStudioBluePrimaryValue, {
+    50: Color(0xFFE3F3FE),
+    100: Color(0xFFBADFFD),
+    200: Color(0xFF8ECCFC),
+    300: Color(0xFF60B8FA),
+    400: Color(0xFF39A8FA),
     500: Color(_ubuntuStudioBluePrimaryValue),
-    600: Color(0xFF008ce0),
-    700: Color(0xFF007cc7),
-    800: Color(0xFF006dae),
-    900: Color(0xFF005d95)
+    600: Color(0xFF008BEB),
+    700: Color(0xFF0079D8),
+    800: Color(0xFF0068C6),
+    900: Color(0xFF004AA7),
   });
 
   // Xubuntu Blue
   static const _xubuntuBluePrimaryValue = 0xFF0044AA;
-  static const xubuntuBlue =
-      MaterialColor(_xubuntuBluePrimaryValue, <int, Color>{
-    50: Color(0xFF80a2d5),
-    100: Color(0xFF668fcc),
-    200: Color(0xFF4d7cc4),
-    300: Color(0xFF3369bb),
-    400: Color(0xFF1a57b3),
-    500: Color(_xubuntuBluePrimaryValue),
-    600: Color(0xFF003d99),
-    700: Color(0xFF003688),
-    800: Color(0xFF003077),
-    900: Color(0xFF002966)
+  static const xubuntuBlue = MaterialColor(_xubuntuBluePrimaryValue, {
+    50: Color(0xFFE2F2FE),
+    100: Color(0xFFBADEFE),
+    200: Color(0xFF8CC9FE),
+    300: Color(0xFF5CB4FD),
+    400: Color(0xFF33A4FE),
+    500: Color(0xFF0094FD),
+    600: Color(0xFF0086EE),
+    700: Color(0xFF0074DB),
+    800: Color(0xFF0063C9),
+    900: Color(_xubuntuBluePrimaryValue),
   });
 }
