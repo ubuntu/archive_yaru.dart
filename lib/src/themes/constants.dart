da487e0fbf0c533da03abc974fa3efdadf794b07
J-P Nurmi <jp.nurmi@canonical.com>
2023-01-12 13:42:08 +0100

    Reduce AppBar height (#243)
    From 48 to 47 to match with YaruTitleBar & GtkHeaderBar.
    
    Fixes: ubuntu/yaru_widgets.dart#502

--- lib/src/themes/constants.dart
+++ lib/src/themes/constants.dart
@@ -4,5 +4,5 @@ const kButtonRadius = 6.0;
 const kCheckRadius = 3.0;
 const kWindowRadius = 8.0;
 const kAppBarElevation = 0.0;
-const kAppBarHeight = 48.0;
+const kAppBarHeight = 47.0;
 const kDisabledGreyDark = Color(0xFF535353);
