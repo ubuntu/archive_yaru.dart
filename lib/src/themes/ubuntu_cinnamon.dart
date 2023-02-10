dbcf81545a64dff5dbdf64574d49d7b072f024e2
J-P Nurmi <jp.nurmi@canonical.com>
2023-02-10 20:25:48 +0100

    Add missing flavors & update example

---
+++ lib/src/themes/ubuntu_cinnamon.dart
@@ -0,0 +1,12 @@
+import 'package:yaru_colors/yaru_colors.dart';
+import 'package:yaru/src/themes/common_themes.dart';
+
+const _primaryColor = YaruColors.ubuntuCinnamonBrown;
+
+final yaruUbuntuCinnamonLight = createYaruLightTheme(
+  primaryColor: _primaryColor,
+);
+
+final yaruUbuntuCinnamonDark = createYaruDarkTheme(
+  primaryColor: _primaryColor,
+);
