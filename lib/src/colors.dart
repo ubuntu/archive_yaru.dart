5a025f3af2dc604d6f57e11d1766a51ecc228bc8
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-04-07 11:29:56 +0200

    remove color pics and links and fix color hex

--- lib/src/colors.dart
+++ lib/src/colors.dart
@@ -5,32 +5,24 @@ class YaruColors {
   YaruColors._();
 
   /// Ubuntu Orange
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/orange.png)
-  static const Color orange = Color(0xFFE95622);
+  static const Color orange = Color(0xFFE95420);
 
   /// Warm Grey
-  ///
   /// For balance. The addition of warm grey softens the combination of orange
   /// and aubergine and provides a bridge between the two.
-  ///
   /// Warm grey can be used for; backgrounds, graphics, dot patterns, charts and
   /// diagrams. It can also be used for large size text.
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/warmGrey.png)
+
   static const Color warmGrey = Color(0xFFAEA79F);
 
   /// Cool Grey
-  ///
   /// For typography, particularly body copy. Black can be quite harsh in
   /// combination with aubergine, but grey delivers more balance while still
   /// being legible.
-  ///
   /// Cool grey can also be used within charts and diagrams.
   static const Color coolGrey = Color(0xFF333333);
 
   /// Text Grey
-  ///
   /// Text grey is used for small size headings, sub-headings and body copy text
   /// only.
   static const Color textGrey = Color(0xFF111111);
@@ -60,92 +52,56 @@ class YaruColors {
   static const Color titleBarDark = Color(0xFF303030);
 
   /// Olive
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/olive.png)
   static const Color olive = Color(0xFF4B8501);
 
   /// Bark
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/bark.png)
   static const Color bark = Color(0xFF787859);
 
   /// Sage
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/sage.png)
   static const Color sage = Color(0xFF657B69);
 
   /// Prussian Green
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/prussianGreen.png)
   static const Color prussianGreen = Color(0xFF308280);
 
   /// Viridian
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/viridian.png)
   static const Color viridian = Color(0xFF03875B);
 
   /// Purple
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/purple.png)
-  static const Color purple = Color(0xFF8856EB);
+  static const Color purple = Color(0xFF7764D8);
 
   /// Red
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/red.png)
-  static const Color red = Color(0xFFE61D34);
+  static const Color red = Color(0xFFDA3450);
 
   /// Blue
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/blue.png)
   static const Color blue = Color(0xFF0073E5);
 
   /// Magenta
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/magenta.png)
-  static const Color magenta = Color(0xFFBC33DB);
+  static const Color magenta = Color(0xFFB34CB3);
 
   /// Kubuntu Blue
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/kubuntuBlue.png)
   static const Color kubuntuBlue = Color(0xFF0079C1);
 
   /// Lubuntu Blue
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/lubuntuBlue.png)
   static const Color lubuntuBlue = Color(0xFF0068C8);
 
   /// Ubuntu Budgie Blue
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/ubuntuBudgieBlue.png)
   static const Color ubuntuBudgieBlue = Color(0xFF2196F3);
 
   /// Ubuntu Butterfly Pink
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/ubuntuButterflyPink.png)
   static const Color ubuntuButterflyPink = Color(0xFFFF135B);
 
   /// Ubuntu Cinnamon Brown
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/ubuntuCinnamonBrown.png)
   static const Color ubuntuCinnamonBrown = Color(0xFFDD682A);
 
   /// Ubuntu MATE Green
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/ubuntuMateGreen.png)
   static const Color ubuntuMateGreen = Color(0xFF73AF59);
 
   /// Ubuntu Studio Blue
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/ubuntuStudioBlue.png)
   static const Color ubuntuStudioBlue = Color(0xFF009BF9);
 
   /// Ubuntu Unity Purple
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/ubuntuUnityPurple.png)
   static const Color ubuntuUnityPurple = Color(0xFF9005D5);
 
   /// Xubuntu Blue
-  ///
-  /// ![](https://raw.githubusercontent.com/ubuntu/yaru.dart/main/doc/images/xubuntuBlue.png)
   static const Color xubuntuBlue = Color(0xFF0044AA);
 }
