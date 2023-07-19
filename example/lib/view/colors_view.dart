63cf7b6d8413128302871f3ca6492d4edddae645
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-07-19 15:16:19 +0200

    display onColor on color if provided

--- example/lib/view/colors_view.dart
+++ example/lib/view/colors_view.dart
@@ -14,7 +14,7 @@ class ColorsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    Map<String, Color> colors = _getAllColors(theme);
+    Map<String, (Color, Color?)> colors = _getAllColors(theme);
 
     final headlineStyle = theme.textTheme.headlineMedium;
 
@@ -37,7 +37,8 @@ class ColorsView extends StatelessWidget {
             final e = colors.entries.elementAt(index);
             return _colorContainer(
               e.key,
-              e.value,
+              e.value.$1,
+              e.value.$2,
             );
           },
         ),
@@ -130,15 +131,21 @@ class ColorsView extends StatelessWidget {
     );
   }
 
-  Widget _colorContainer(String colorName, Color color) {
+  Widget _colorContainer(
+    String colorName,
+    Color backgroundColor, [
+    Color? foregroundColor,
+  ]) {
     return Container(
-      decoration:
-          BoxDecoration(color: color, borderRadius: BorderRadius.circular(6)),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(6),
+      ),
       child: Center(
         child: Text(
           colorName,
           style: TextStyle(
-            color: contrastColor(color),
+            color: foregroundColor ?? contrastColor(backgroundColor),
           ),
         ),
       ),
@@ -160,41 +167,41 @@ class _SpacedDivider extends StatelessWidget {
   }
 }
 
-Map<String, Color> _getAllColors(ThemeData theme) {
+Map<String, (Color, Color?)> _getAllColors(ThemeData theme) {
   final s = theme.colorScheme;
 
-  final colors = <String, Color>{
-    'primaryColor': theme.primaryColor,
-    'primary': s.primary,
-    'onPrimary': s.onPrimary,
-    'primaryContainer': s.primaryContainer,
-    'onPrimaryContainer': s.onPrimaryContainer,
-    'secondary': s.secondary,
-    'onSecondary': s.onSecondary,
-    'secondaryContainer': s.secondaryContainer,
-    'onSecondaryContainer': s.onSecondaryContainer,
-    'tertiary': s.tertiary,
-    'onTertiary': s.onTertiary,
-    'tertiaryContainer': s.tertiaryContainer,
-    'onTertiaryContainer': s.onTertiaryContainer,
-    'error': s.error,
-    'onError': s.onError,
-    'errorContainer': s.errorContainer,
-    'onErrorContainer': s.onErrorContainer,
-    'outline': s.outline,
-    'outlineVariant': s.outlineVariant,
-    'background': s.background,
-    'onBackground': s.onBackground,
-    'surface': s.surface,
-    'onSurface': s.onSurface,
-    'surfaceVariant': s.surfaceVariant,
-    'onSurfaceVariant': s.onSurfaceVariant,
-    'inverseSurface': s.inverseSurface,
-    'onInverseSurface': s.onInverseSurface,
-    'inversePrimary': s.inversePrimary,
-    'shadow': s.shadow,
-    'scrim': s.scrim,
-    'surfaceTint': s.surfaceTint,
+  final colors = <String, (Color, Color?)>{
+    'primaryColor': (theme.primaryColor, null),
+    'primary': (s.primary, s.onPrimary),
+    'onPrimary': (s.onPrimary, null),
+    'primaryContainer': (s.primaryContainer, s.onPrimaryContainer),
+    'onPrimaryContainer': (s.onPrimaryContainer, null),
+    'secondary': (s.secondary, s.onSecondary),
+    'onSecondary': (s.onSecondary, null),
+    'secondaryContainer': (s.secondaryContainer, s.onSecondaryContainer),
+    'onSecondaryContainer': (s.onSecondaryContainer, null),
+    'tertiary': (s.tertiary, s.onTertiary),
+    'onTertiary': (s.onTertiary, null),
+    'tertiaryContainer': (s.tertiaryContainer, s.onTertiaryContainer),
+    'onTertiaryContainer': (s.onTertiaryContainer, null),
+    'error': (s.error, s.onError),
+    'onError': (s.onError, null),
+    'errorContainer': (s.errorContainer, s.onErrorContainer),
+    'onErrorContainer': (s.onErrorContainer, null),
+    'outline': (s.outline, null),
+    'outlineVariant': (s.outlineVariant, null),
+    'background': (s.background, s.onBackground),
+    'onBackground': (s.onBackground, null),
+    'surface': (s.surface, s.onSurface),
+    'onSurface': (s.onSurface, null),
+    'surfaceVariant': (s.surfaceVariant, s.onSurfaceVariant),
+    'onSurfaceVariant': (s.onSurfaceVariant, null),
+    'inverseSurface': (s.inverseSurface, s.onInverseSurface),
+    'onInverseSurface': (s.onInverseSurface, null),
+    'inversePrimary': (s.inversePrimary, null),
+    'shadow': (s.shadow, null),
+    'scrim': (s.scrim, null),
+    'surfaceTint': (s.surfaceTint, null),
   };
   return colors;
 }
